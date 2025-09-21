372f71ecdc8a142f51b431e6502609638d80feef
joaop9673 <53797640+joaop9673@users.noreply.github.com>
2025-09-21 12:07:22 -0300

    migrando pro hugo

--- themes/w3css/exampleSite/static/src/demo-hugo-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0)/..
-# hugo server -w --themesDir ../../ --disableFastRender --navigateToChanged
-hugo server -e example -w --themesDir ../../ --disableFastRender --navigateToChanged
